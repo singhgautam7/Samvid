524bb930b78739e17aacd5b81df39c88bba9cffe
Gautam <gautamsingh1997@gmail.com>
2017-10-19 01:57:44 +0530

    Added appropriate icons for MainActivity
    All the appropriate icons are added to the MainActivity(NavigationBar Activity) and removed unwaned icons.

--- app/src/main/java/com/teamsix/samvid/MainActivity.java
+++ app/src/main/java/com/teamsix/samvid/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity
         setSupportActionBar(toolbar);
 
         //can use snackbar too
-        
+
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
