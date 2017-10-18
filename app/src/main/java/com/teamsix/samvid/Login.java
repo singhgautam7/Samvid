fee2d63f92e280557c732b3c5a14aae0d70b26eb
Gautam <gautamsingh1997@gmail.com>
2017-10-18 17:48:33 +0530

    Re-designed login menu, added ripple effects on buttons, added intent on skip button

--- app/src/main/java/com/teamsix/samvid/Login.java
+++ app/src/main/java/com/teamsix/samvid/Login.java
@@ -1,13 +1,30 @@
 package com.teamsix.samvid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ButtonBarLayout;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+
+    Button skip;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        skip=(Button)findViewById(R.id.textView2);
+
+        skip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Login.this,MainActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
