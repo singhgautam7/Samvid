03c47e94cf3c8891cfb7308b9d6c54980e26d1e1
Gautam <gautamsingh1997@gmail.com>
2017-10-26 04:39:36 +0530

    Added one more FAB in FragmentEvents for the bottom sheet filters
    All the animations are also applied in the smallFAB and FAB for the new controls

--- app/src/main/java/com/teamsix/samvid/FragmentEvents.java
+++ app/src/main/java/com/teamsix/samvid/FragmentEvents.java
@@ -1,11 +1,11 @@
 package com.teamsix.samvid;
 
+import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomSheetBehavior;
-import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
@@ -13,9 +13,7 @@ import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.animation.AccelerateInterpolator;
 import android.view.animation.Animation;
-import android.view.animation.DecelerateInterpolator;
 import android.view.animation.ScaleAnimation;
 
 /**
@@ -33,29 +31,41 @@ public class FragmentEvents extends Fragment {
 
         View bottomSheet = view.findViewById(R.id.bottom_sheet);
         mBottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
-        //By default set BottomSheet Behavior as Collapsed and Height 0
         mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
         mBottomSheetBehavior.setPeekHeight(0);
 
         final FloatingActionButton fab=(FloatingActionButton)view.findViewById(R.id.fab);
-        fab.setVisibility(View.VISIBLE);
+        final FloatingActionButton smallfab=(FloatingActionButton)view.findViewById(R.id.smallfab);
 
         view.post(new Runnable() {
             @Override
             public void run() {
                 view.requestLayout();
             }
-        });
+        });         //to proprly anchor main FAB everytime when the control returns to this fragment
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (mBottomSheetBehavior.getState() == BottomSheetBehavior.STATE_COLLAPSED)
-                    //If state is in collapse mode expand it
+                if (mBottomSheetBehavior.getState() == BottomSheetBehavior.STATE_COLLAPSED) {
                     mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
-                else
-                    //else if state is expanded collapse it
+                    changeFABdesign(Color.parseColor("#00C853") , R.drawable.ic_done_white);
+                    smallFABvisibility("SHOW");
+                }
+                else {
                     mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                    changeFABdesign(getResources().getColor(R.color.colorAccent) , R.drawable.ic_filter_list_white);
+                    smallFABvisibility("HIDE");
+                }
+            }
+        });
+
+        smallfab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                changeFABdesign(getResources().getColor(R.color.colorAccent) , R.drawable.ic_filter_list_white);
+                smallFABvisibility("HIDE");
             }
         });
 
@@ -63,11 +73,8 @@ public class FragmentEvents extends Fragment {
         ViewPager viewPager = (ViewPager)view.findViewById(R.id.view_pager);
         viewPager.setAdapter(new CustomFragmentPageAdapter(getChildFragmentManager()));
         tabLayout.setupWithViewPager(viewPager);
-        //returning our layout file
-        //change R.layout.yourlayoutfilename for each of your fragments
-        // Create an instance of the tab layout from the view.
+
         tabLayout = (TabLayout) view.findViewById(R.id.tabs);
-        // Set the text for each tab.
         tabLayout.getTabAt(0).setIcon(R.drawable.ic_all_events_white);
         tabLayout.getTabAt(1).setIcon(R.drawable.ic_star);
         // Set the tabs to fill the entire layout.
@@ -110,9 +117,9 @@ public class FragmentEvents extends Fragment {
     void animateFab(int position,final FloatingActionButton fab) {
         fab.clearAnimation();
         ScaleAnimation shrink =  new ScaleAnimation(1f, 0, 1f, 0, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
-        shrink.setDuration(150);     // animation duration in milliseconds
+        shrink.setDuration(125);
         ScaleAnimation expand =  new ScaleAnimation(0, 1f, 0, 1f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
-        expand.setDuration(150);     // animation duration in milliseconds
+        expand.setDuration(125);
 
         switch (position) {
             case 0:
@@ -121,28 +128,73 @@ public class FragmentEvents extends Fragment {
                 break;
             case 1:
                 fab.startAnimation(shrink);
-                Handler mHandler=new Handler();
+                final Handler mHandler=new Handler();
                 mHandler.postDelayed(new Runnable() {
                     @Override
                     public void run() {
                         fab.setVisibility(View.INVISIBLE);
+                        fab.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.colorAccent)));
+                        fab.setImageResource(R.drawable.ic_filter_list_white);
                     }
-                },150);
+                },125);
+                if (mBottomSheetBehavior.getState() == BottomSheetBehavior.STATE_EXPANDED) {        //bottom sheet expanded condition used slightly differnetly
+                    mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                    smallFABvisibility("HIDE");
+                }
                 break;
-                default:
-                    fab.setVisibility(View.VISIBLE);
-                    fab.startAnimation(expand);
-                    break;
+            default:
+                fab.setVisibility(View.VISIBLE);
+                fab.startAnimation(expand);
+                break;
         }
     }
 
-    @Override
-    public void onPause() {
-        super.onPause();
+    void changeFABdesign(final int color ,final int icon )
+    {
+        final FloatingActionButton fab=(FloatingActionButton)getView().findViewById(R.id.fab);
+        fab.clearAnimation();
+        final ScaleAnimation shrink =  new ScaleAnimation(1f, 0.2f, 1f, 0.2f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        shrink.setDuration(125);
+        final ScaleAnimation expand =  new ScaleAnimation(0.2f, 1f, 0.2f, 1f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        expand.setDuration(125);
+        final Handler mHandler = new Handler();
+
+        fab.startAnimation(shrink);
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                fab.setBackgroundTintList(ColorStateList.valueOf(color));
+                fab.setImageResource(icon);
+                fab.startAnimation(expand);
+            }
+        },125);
+
     }
 
-    @Override
-    public void onResume() {
-        super.onResume();
+    void smallFABvisibility(String str)
+    {
+        final FloatingActionButton smallfab=(FloatingActionButton)getView().findViewById(R.id.smallfab);
+        smallfab.clearAnimation();
+        final ScaleAnimation shrink =  new ScaleAnimation(1f, 0.2f, 1f, 0.2f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        shrink.setDuration(250);
+        final ScaleAnimation expand =  new ScaleAnimation(0.2f, 1f, 0.2f, 1f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        expand.setDuration(250);
+        final Handler mHandler = new Handler();
+
+        if (str.equals("SHOW")) {
+            smallfab.setVisibility(View.INVISIBLE);
+            smallfab.startAnimation(expand);
+            smallfab.setVisibility(View.VISIBLE);
+        }
+
+        else if (str.equals("HIDE")) {
+            smallfab.startAnimation(shrink);
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    smallfab.setVisibility(View.GONE);
+                }
+            },250);
+        }
     }
 }
